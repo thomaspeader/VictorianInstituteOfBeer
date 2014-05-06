12f34f28e434fb7d70ad8db8c9715fca7e8dfae4
hailhypnotoad <jclyons52@gmail.com>
2014-05-06 21:18:43 +1000

    externalized css, added header. minor style changes to various elements

---
+++ header.php
@@ -0,0 +1,28 @@
+<head>
+	<style type="text/css">
+	#header{
+		width: 100%;
+		height: 160px;
+		background-color: #FFFFFF;
+		color: #000000
+		text-align: left;
+	}
+	#divider{
+		background-color: #000000;
+		width: 100%;
+		height: 40px;
+	}
+	</style>
+
+</head>
+
+
+<div id="header">
+	<h1>
+		VICTORIAN<br>
+		INSTITUTE<br>
+		OF BEER
+	</h1>
+
+</div>
+<div id="divider"></div>
--- menuBar.php
+++ menuBar.php
@@ -72,10 +72,10 @@
 </head>
 <nav>
 <ul>
-    <li><a href="index.html">about us</a></li>
-    <li style= "margin-left: 20%"><a href="">beer club</a></li>
-    <li style= "margin-left: 40%"><a href="gallery.html">gallery</a></li>
-    <li style= "margin-left: 60%"><a href="contactForm.html">contact Page</a></li>
+    <li style= "margin-left: 40%"><a href="index.html">about us</a></li>
+    <li style= "margin-left: 50%"><a href="">beer club</a></li>
+    <li style= "margin-left: 60%"><a href="gallery.html">gallery</a></li>
+    <li style= "margin-left: 70%"><a href="contactForm.html">contact Page</a></li>
     <li style= "margin-left: 80%"><details>
     <summary>breweries</summary>
     <a href="#">Subcategory A</a>

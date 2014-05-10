e82d09fe1a3a0676f56f961a4824a3b75f5f3544
joseph lyons <jclyons52@gmail.com>
2014-05-10 14:37:44 +1000

    created breweries page with separate css, removed remaining inline styles. in the future a global css sheet will be needed as many more page specific sheets will be needed

--- header.php
+++ header.php
@@ -1,22 +1,3 @@
-<head>
-	<style type="text/css">
-	#header{
-		width: 100%;
-		height: 160px;
-		background-color: #FFFFFF;
-		color: #000000
-		text-align: left;
-	}
-	#divider{
-		background-color: #000000;
-		width: 100%;
-		height: 40px;
-	}
-	</style>
-
-</head>
-
-
 <div id="header">
 	<h1>
 		VICTORIAN<br>
--- menuBar.php
+++ menuBar.php
@@ -1,74 +1,5 @@
 <head>
-<style type="text/css">
-    nav{
-        background-color: #333333;
-        width: 100%;
-        height: 60px;
-    }
-    nav a:visited{
-        color: #999
-    }
-    nav li{
-        position: absolute;
-        padding: 20px;
-        border: #999 2px dashed
-        white-space: nowrap;
-        width: 15%;
-        min-width: 150px;
 
-
-    }
-   nav a{
-        padding: 12px 20px;
-        color:#999;
-        background: #333;
-        -webkit-transition: background 0.3s linear 0s, color 0.3s linear 0s;
-        -moz-transition: background 0.3s linear 0s, color 0.3s linear 0s;
-        -ms-transition: background 0.3s linear 0s, color 0.3s linear 0s;
-        -o-transition: background 0.3s linear 0s, color 0.3s linear 0s;
-        transition: background 0.3s linear 0s, color 0.3s linear 0s;
-    }
-    nav a:hover{
-        background: #C41230;
-        color:#FFF;
-    }
-    nav details{
-        position: absolute;
-    }
-    nav details > summary{
-        z-index: 10;
-        cursor:pointer;
-        background: #333;
-        margin-top:-26px;
-        padding:8px;
-        color:#FFF;
-        -webkit-transition: background 0.3s linear 0s, color 0.3s linear 0s;
-        -moz-transition: background 0.3s linear 0s, color 0.3s linear 0s;
-        -ms-transition: background 0.3s linear 0s, color 0.3s linear 0s;
-        -o-transition: background 0.3s linear 0s, color 0.3s linear 0s;
-        transition: background 0.3s linear 0s, color 0.3s linear 0s;
-    }
-    nav details > summary:hover{
-        background: #C41230;
-    }
-    nav details > summary::-webkit-details-marker{
-        /*display: none;*/
-    }
-    nav details > a{
-        display: block;
-        text-decoration: none;
-        color:#999;
-        font-size:13px;
-        background: #333;
-    }
-    nav details > a:hover{
-        background: #C41230;
-        font-weight: bold;
-    }
-    nav details > a:before{
-        content: "- ";
-    }
-</style>
 </head>
 <nav>
 <ul>
@@ -78,7 +9,7 @@
     <li style= "margin-left: 70%"><a href="contactForm.html">contact Page</a></li>
     <li style= "margin-left: 80%"><details>
     <summary>brewries</summary>
-    <a href="#">Subcategory A</a>
+    <a href="brewries.html">matilda bay</a>
     <a href="#">Subcategory B</a>
     <a href="#">Subcategory C</a>
     </details></li>
